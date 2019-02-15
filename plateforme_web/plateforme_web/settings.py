0e4d3bfa0c5e12ab61aea6a8359f5f98177b8d1c
ferreira <guillaumeferreira00@gmail.com>
2019-02-15 15:32:12 +0100

    Update settings.py

--- plateforme_web/plateforme_web/settings.py
+++ plateforme_web/plateforme_web/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'test'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["0.0.0.0","https://plateformeweb1.herokuapp.com/"]
 
