6630fce576f5b87d28c7ada9e2f6e28bd263a69f
ferreira <guillaumeferreira00@gmail.com>
2019-02-15 06:27:15 +0100

    essaie pour heroku

--- plateforme_web/plateforme_web/settings.py
+++ plateforme_web/plateforme_web/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = '_mi-*&v07ibo=k=8s$8==d1+5(l-bb2#$_4@)6^@8-^mi*#!rq'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['0.0.0.0', '127.0.0.1','https://plateformeweb1.herokuapp.com']
+ALLOWED_HOSTS = ['0.0.0.0', '127.0.0.1','https://plateformeweb1.herokuapp.com','86.195.177.229']
 
 
 # Application definition
