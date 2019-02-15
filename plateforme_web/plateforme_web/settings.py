400800ddea2da16562dd15082618db3a189030f0
ferreira <guillaumeferreira00@gmail.com>
2019-02-15 09:46:32 +0100

    Update settings.py

--- plateforme_web/plateforme_web/settings.py
+++ plateforme_web/plateforme_web/settings.py
@@ -129,12 +129,12 @@ INTERNAL_IPS=['0.0.0.0']
 
 # Static files settings
 
-#PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
-"""
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
     os.path.join(PROJECT_ROOT, 'static'),
 )
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'"""
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
